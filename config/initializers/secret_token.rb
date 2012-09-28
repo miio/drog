d1c5dbd25fa7137c9159c728d1276bfd508a9f4b
miio <info@miio.info>
2012-09-28 23:23:41 +0900

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Drog::Application.config.secret_token = '2f504f0db279af01965b1fb57eaea42e4a6661a592a9feafd004ce89709ca42f1962d1e37ff3bcfcebdd3f672d084495e1410294642c2804856c7103616fd24e'
