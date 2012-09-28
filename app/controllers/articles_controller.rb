65309cf248fd4aa0726aede537600abd1b31fb86
miio <info@miio.info>
2012-09-29 03:07:10 +0900

    add article form and basic logic

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,9 @@
 class ArticlesController < InheritedResources::Base
+  before_filter :assign_author, only: [:create]
+  actions :index, :show, :new, :create, :delete
+  respond_to :html, :xml, :json
+
+  def assign_author
+    build_resource.author = current_user
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,11 +1,12 @@
 class Article < ActiveRecord::Base
-  attr_accessible :title, :body
+  attr_accessible :title, :body, :updated_at, :created_at
   belongs_to :author, class_name: "User"
   belongs_to :category
   has_many :comments
   validates_presence_of :author
 
   def save
-    write_attribute('author', current_user) if respond_to?(:author) && user.nil?
+    write_attribute('author', current_user) if respond_to?(:author) && author.nil?
+    super
   end
 end
